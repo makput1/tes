67363e86dc93c5e9b63ed35e2ca30cb2ec8fa8d5
makput1 <pkuypkuy6@gmail.com>
2023-05-09 21:43:12 +0700

    p

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("tes bang");
+const {
+  default: makeWASocket,
+  useSingleFileAuthState,
+} = require("@adiwajshing/baileys");
+const pino = require("pino");
+const { state, saveState } = useSingleFileAuthState("session.json");
+
+const startBot = async () => {
+  const sock = makeWASocket({
+    printQRInTerminal: true,
+    auth: state,
+    logger: pino({ level: "silent" }),
+  });
+  sock.ev.on("creds.update", saveState);
+};
+
+startBot();
