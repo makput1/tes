424e7e645622cbba0ec674ffa0e00509ccb7aed1
makput1 <pkuypkuy6@gmail.com>
2023-05-09 20:54:00 +0700

    p

---
+++ index.js
@@ -0,0 +1,51 @@
+const {
+  default: makeWASocket,
+  DisconnectReason,
+  useSingleFileAuthState,
+  extractMessageContent,
+  makeInMemoryStore,
+} = require("@adiwajshing/baileys");
+const { Boom } = require("@hapi/boom");
+const logg = require("pino");
+const { state, saveState } = useSingleFileAuthState("sesi.json");
+
+const startBot = () => {
+  const sock = makeWASocket({
+    printQRInTerminal: true,
+    auth: state,
+    logger: logg({ level: "silent" }),
+  });
+
+  sock.ev.on("creds.update", saveState);
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      const shouldReconnect = (lastDisconnect.error =
+        Boom?.output?.statusCode !== DisconnectReason.loggedOut);
+      if (shouldReconnect) {
+        startBot();
+      }
+    } else if (connection === "open") {
+      console.log("koneksi open");
+    }
+  });
+  sock.ev.on("presence.update", (json) => console.log(json));
+  sock.ev.on("messages.upsert", async (chatUpdate) => {
+    var msg = chatUpdate.messages[0];
+    var chat = msg.message.conversation;
+    var sender = msg.key.remoteJid;
+    const keys = {
+      remoteJid: msg.key.remoteJid,
+      id: msg.key.id,
+    };
+    if (chat) {
+      sock.readMessages([keys]);
+    }
+    if (chat === ".start") {
+      sock.sendMessage(sender, { text: "selamat datang" });
+    }
+    //console.log(msg);
+  });
+};
+
+startBot();
